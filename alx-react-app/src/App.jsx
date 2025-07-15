8db1fdf5dd37282fcc7f186e222af337bfb38ac3
Jerome Maunga <jerhomemaunga@gmail.com>
2025-07-15 15:32:20 +0300

    Pre-built Component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from '../../../src/components/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
