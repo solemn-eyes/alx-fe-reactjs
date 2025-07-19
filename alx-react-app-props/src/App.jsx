0aa300c2634e5988daa0e3f805bd01a896813dfd
Jerome Maunga <jerhomemaunga@gmail.com>
2025-07-19 19:31:04 +0300

    Another one Again!

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,7 @@
 import ProfilePage from './ProfilePage';
 import React from 'react';
 import UserContext from './UserContext';
+import UserProfile from './components/UserProfile';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
