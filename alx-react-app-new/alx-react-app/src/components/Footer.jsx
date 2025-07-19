7ea4c50682c9b5734eeea69e4451f68849f500ba
Jerome Maunga <jerhomemaunga@gmail.com>
2025-07-19 14:46:45 +0300

    Applying Inline CSS styling

---
+++ alx-react-app-new/alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: 'lightgray', textAlign: 'center', padding: '10px' }}>
+            <p style={{ fontWeight: 'bold', fontStyle: 'italic', color: 'black'}}>&copy; 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', backgroundColor: 'black', color: 'white' }}>
+            <p style={{ fontFamily: 'fantasy'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
