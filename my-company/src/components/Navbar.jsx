22a53647b2f5296f3e86b00b8a38064facb723c2
Jerome Maunga <jerhomemaunga@gmail.com>
2025-07-20 11:36:37 +0300

    A simple Company Website with React

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
-        <nav style={{ padding: '1rem', background: '#f2f2f2' }}>
+        <nav style={{ backgroundColor: '#282c34', display: 'flex', justifyContent: 'space-around', padding: '10px' }}>
             <Link to="/" style={{ marginRight:'1rem' }}>Home</Link>
             <Link to="/about" style={{ marginRight:'1rem' }}>About</Link>
             <Link to="services" style={{ marginRight:'1rem' }}>Services</Link>
