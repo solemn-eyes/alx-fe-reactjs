01ec3b5f9be69827bedc9012ab4cd340e066b930
Jerome Maunga <jerhomemaunga@gmail.com>
2025-07-26 17:48:35 +0300

    Another simple change

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
   // RecipeList component
-  import useRecipeStore from '../recipeStore';
+  import useRecipeStore from './recipeStore';
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
