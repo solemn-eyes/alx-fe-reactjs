3b015307ae48db6e9e0338f89db51cc5b4892de6
Jerome Maunga <jerhomemaunga@gmail.com>
2025-08-02 10:41:38 +0300

    Setting up a React Project for GitHub User Search App

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function Home() {
+  return <h1>Home Page</h1>
+}
+
+function About() {
+  return <h1>About Page</h1>
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div>
+              <a href="https://vite.dev" target="_blank">
+                <img src={viteLogo} className="logo" alt="Vite logo" />
+              </a>
+              <a href="https://react.dev" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo" />
+              </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+              </button>
+              <p>
+                Edit <code>src/App.jsx</code> and save to test HMR
+              </p>
+            </div>
+            <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+            </p>
+          </>
+        } />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
