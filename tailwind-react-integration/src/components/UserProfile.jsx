a76b65c541863a399c0c1ceeda697a80c9fcc8c6
Jerome Maunga <jerhomemaunga@gmail.com>
2025-08-05 16:12:10 +0300

    Making it a responsive design

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
     return (
-        <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
+        <div className="user-profile bg-gray-100 sm:p-4 md:p-8 md:max-w-sm sm:max-w-xs mx-auto my-20 rounded-lg shadow-lg">
             <img src="https://via.placeholder.com/150" alt="User" className="rounded-full md:w-36 md:h-36 sm:w-24 sm:h-24 mx-auto" />
             <h1 className="md:text-xl sm:text-lg text-blue-800 my-4" >John Doe</h1>
             <p className="text-gray-600 md:text-base sm:text-sm" >Developer at Example Co. Loves to write code and explore new technologies.</p>
