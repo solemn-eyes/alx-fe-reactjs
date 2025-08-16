cd57be706a67432afe21ad0160247a836dd7f91c
Jerome Maunga <jerhomemaunga@gmail.com>
2025-08-16 09:45:35 +0300

    Setting up a project with React and Tailwindcss

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class' if needed
   theme: {
     extend: {
