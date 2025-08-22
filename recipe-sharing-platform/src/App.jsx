e8c7fa78c7cbfac0a33396e24a2bb22ab6b945b6
Jerome Maunga <jerhomemaunga@gmail.com>
2025-08-22 12:54:56 +0300

    HomePage

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
-import Homepage from './components/Homepage'
+import Recipe from './components/Homepage'
 
 function App() {
   
   return (
     <>
      <div>
-      <h1>Just testing!</h1>
-      <Homepage />
+      <Recipe />
      </div>
       
     </>
--- recipe-sharing-platform/src/components/Homepage.jsx
+++ recipe-sharing-platform/src/components/Homepage.jsx
@@ -1,22 +1,29 @@
-import { useState, useEffect} from "react"
 
-const Homepage = () => {
-    const [data, setData] = useState([]);
+import { useState, useEffect } from "react";
 
-    useEffect(() => {
-        fetch("./src/data.json")
+function Recipe(){
+    const[recipe, setRecipe]= useState([]);
+
+    useEffect(()=>{
+        fetch("/data.json")
             .then(response => response.json())
-            .then(data => setData(data))
-            .catch(error => console.error("Error fetching data:", error));
+            .then(data => setRecipe(data))
     }, []);
 
     return (
-        <div>
-            <h1>Homepage</h1>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-            
+        <div className="content-center justify-items-center p-4">
+            <h1 className="text-3xl font-bold mb-6">Recipe</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                {recipe.map((recipe, index) => (
+                    <div key={index} className="bg-white rounded-lg shadow-md p-4">
+                        <h2 className="text-black text-xl font-semibold mb-2">{recipe.title}</h2>
+                        <p className="text-black mb-2">{recipe.summary}</p>
+
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
 
-export default Homepage;
+export default Recipe;
