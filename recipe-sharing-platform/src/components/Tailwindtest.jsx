f0ee991097b97db3dd497ecf47e3b9949f778762
Jerome Maunga <jerhomemaunga@gmail.com>
2025-08-22 12:17:08 +0300

    HomePage

--- recipe-sharing-platform/src/components/Tailwindtest.jsx
+++
@@ -1,9 +0,0 @@
-function Testing(){
-    return (
-        <div className="bg-gray-600">
-            <p className="text-blue-500 underline text-6xl">Just setting up TailwindCSS in the project!!!</p>
-        </div>
-    );
-}
-
-export default Testing;
